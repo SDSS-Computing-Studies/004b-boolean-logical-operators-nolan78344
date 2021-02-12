1b4d4260e6a8a3b0bb561d622c2461db227d1c53
nolano78344 <55156573+nolan78344@users.noreply.github.com>
2021-02-12 15:25:26 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -34,7 +34,7 @@ a = float(input("Please enter number "))
 if (math.sqrt(a)) == (round(math.sqrt(a))) and (round((a**(1/3)),4)) == round(a**(1/3)):
     print (str(a) +" is both a perfect square and a perfect cube.")
 elif round((a**(1/3)),4) == round(a**(1/3)):
-    print (str(a) +" is only a perfect square.")
-elif (math.sqrt(a),4) == round(math.sqrt(a)):
     print (str(a) +" is only a perfect cube.")
+elif (math.sqrt(a)) == round(math.sqrt(a)):
+    print (str(a) +" is only a perfect square.")
 
